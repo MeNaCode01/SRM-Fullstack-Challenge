baa6a1f1df281ed66ca93af7963dcabc9401a82b
MeNaCode01 <manavcharia10@gmail.com>
2026-04-24 19:23:43 +0530

    Set frontend API URL

--- srm-challenge-frontend/script.js
+++ srm-challenge-frontend/script.js
@@ -2,7 +2,7 @@ const API_BASE =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://YOUR-RENDER-APP.onrender.com";
+    : "https://srm-fullstack-iota.vercel.app";
 
 document.getElementById("submitBtn").addEventListener("click", async () => {
   const inputEl = document.getElementById("jsonInput");
